4c4d5d9e90e4b3e3032bbebfd063c7dbe956e638
umairgillani93 <umairgillani93@gmail.com>
2022-10-01 01:16:23 +0500

    bert-ner model

--- nlp_for_transformers/ner/ner_model.py
+++ nlp_for_transformers/ner/ner_model.py
@@ -1,32 +1,52 @@
 import os
 import pickle 
+import torch
 import numpy as np 
 import pandas as pd
 from transformers import pipeline 
+from nltk.tokenize.treebank import TreebankWordDetokenizer
 
+import transformers
+
+print('torch version: {}'.format(torch.__version__))
+print('transformers version: {}'.format(transformers.__version__))
+print('\nCuda Available: {}'.format(torch.cuda.is_available()))
 print('\nImports successful')
 path = os.getenv('HOME') + '/ner_pretrained_weights/'
+print(f'\nPath: {path}')
 
-print('\n >> Downloading model')
-ner = pipeline('ner', path, aggregation_strategy='simple', device=0)
-
+print('\n >> Loading model')
+ner = pipeline('ner', path, aggregation_strategy='simple')
+#print('\nSaving model path: {}'.format(path))
+#ner.save_pretrained(path)
+print('\nModel saved')
+#ner = pipeline('ner', path)
+print('\nMOdel loaded!')
 with open('ner_train.pkl', 'rb') as f:
   corpus_train = pickle.load(f)
 
 with open('ner_test.pkl', 'rb') as f:
   corpus_test = pickle.load(f)
   
-print('\ncorpus test: {}'.format(corpus_test))
-
-inputs - []
-targets - []
+inputs = []
+targets = []
 
 for stp in corpus_test:
   tokens = []
-  targest = []
+  target = []
   for token, tag in stp:
     tokens.append(token)
-    targest.append(tag)o
+    target.append(tag)
   inputs.append(tokens)
-  targest.append(target)
+  targets.append(target)
+
+print('\nInputs: {}'.format(inputs[9]))
+
+# for joining / detokenizing the work tokens
+detokenizer = TreebankWordDetokenizer()
+
+print(detokenizer.detokenize(inputs[9]))
+print('\ntargets: {}'.format(targets[9]))
+print(ner(detokenizer.detokenize(inputs[9])))
+
 
