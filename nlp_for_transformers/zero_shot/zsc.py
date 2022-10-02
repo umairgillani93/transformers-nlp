8f1aff556206c84dad85fd73fbb2e00d7ea86dd9
umairgillani93 <umairgillani93@gmail.com>
2022-10-03 04:25:16 +0500

    bert zero-shot classifier

---
+++ nlp_for_transformers/zero_shot/zsc.py
@@ -0,0 +1,41 @@
+import os
+import torch
+import textwrap
+import pprint
+import numpy as np
+import pandas as pd 
+import matplotlib.pyplot as plt
+from transformers import pipeline 
+from sklearn.metrics import roc_auc_score, f1_score, confusion_matrix
+import warnings
+warnings.filterwarnings('ignore')
+
+print('Imports successful')
+
+path = os.getenv('HOME') + '/models/zero_shot_pretrained_weights/'
+print('\n >> Loading model..')
+# An advantage of zero-shot classification model is
+# It can take any input and try to classify it
+# Whether it's an image or a text sample
+# So it's useful for labeling the image and text files as it doesn't require labels at the begining
+
+classifier = pipeline('zero-shot-classification', path)
+print('\nMOdel Loaded')
+print('\n')
+print(classifier("this is a great movie", candidate_labels=['positive', 'negative']))
+
+text = """
+  Due to the presence of isoforms of its components, there are 12 
+  versions of AMPK in mammals
+  """
+print('\n')
+print(classifier(text, candidate_labels=['biology', 'math', 'geology']))
+
+# load the dataset
+df = pd.read_csv('../text_summarizer/bbc_text_cls.csv')
+
+print('\n', df.head())
+
+
+
+
