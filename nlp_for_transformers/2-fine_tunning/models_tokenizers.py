89bb2a9a3688530cdfbef03799ed090e615e0c29
umairgillani93 <umairgillani93@gmail.com>
2022-10-03 19:51:24 +0500

    BERT models and tokenizers

--- nlp_for_transformers/2-fine_tunning/models_tokenizers.py
+++ nlp_for_transformers/2-fine_tunning/models_tokenizers.py
@@ -3,6 +3,17 @@ import pprint
 from transformers import AutoTokenizer
 from transformers import AutoModelForSequenceClassification
 
+def save_model(path, model, chkpt):
+  '''
+  downloads and saves the model from Huggingface.co
+  '''
+  print('\n >> Downloading model ..')
+  model_ = model.from_pretrained(chkpt)
+  print('\n >> Saving model to path: {}'.format(path))
+  model_.save_pretrained(path)
+  print('\nMOdel saved!')
+
+
 # prettry printer for python
 pp = pprint.PrettyPrinter(indent=4)
 
@@ -10,11 +21,12 @@ pp = pprint.PrettyPrinter(indent=4)
 # remember! this should be same for both 'Tokenizer' and 'Model'
 checkpoint = 'bert-base-uncased'
 # path to save the tokenizer
-path = os.getenv('HOME') + '/tokenizers/AutoTokenizer/'
+tokenizer_path = os.getenv('HOME') + '/tokenizers/AutoTokenizer/'
+model_path = os.getenv('HOME') + '/models/AutoModelForSequenceClassification_pretrained/'
 
 # instantiate the tokenizer object
 print('\n >> Loading Tokenizer..')
-tokenizer = AutoTokenizer.from_pretrained(path)
+tokenizer = AutoTokenizer.from_pretrained(tokenizer_path)
 
 #tokenizer.save_pretrained(path)
 #print('\nTokenizer saved at path: {}'.format(path))
@@ -46,14 +58,41 @@ print('\nDecode: {}'.format(tokenizer.decode(tokenizer.encode(text))))
 # Model inputs
 # REturns the dictionary of input_ids -> integer ids for each token
 # token_type_ids and attention mask
-model_inputs = tokenizer(text)
-pp.pprint('Model inputs: {}'.format(model_inputs))
+#model_inputs = tokenizer(text)
+#pp.pprint('Model inputs: {}'.format(model_inputs))
 
 data = [
-    'sky is blue',
-    'NLP is my craze'
+    'test example number 1',
+    'test exammple number 2'
     ]
 
 
 pp.pprint('Data tokenizer: {}'.format(tokenizer(data)))
 pp.pprint('Decode Data tokenizer: {}'.format(tokenizer.decode(tokenizer(data)['input_ids'][0])))
+
+
+model_inputs = tokenizer(data, return_tensors='pt', truncation=True, padding=True) 
+print('\nMOdel inputs', model_inputs)
+
+#save_model(model_path, AutoModelForSequenceClassification, checkpoint)
+
+model = AutoModelForSequenceClassification.from_pretrained(model_path, 
+    num_labels=2)
+    
+print('\nSequence Model loaded successfully')
+
+print('\nModel labels: {}'.format(model.num_labels))
+
+# output of the model returns named_tuple / dictionary
+# we can get each item by indexing, output['labels'] dictionary key grabbing way or simply by output.logis dot notation
+outputs = model(**model_inputs)
+print('\nModel outputs: {}'.format(outputs))
+
+# remove the grad from logits in order to calculte the mean, probabilities etc
+print('detached logits: {}'.format(outputs.logits.detach().numpy()))
+
+model_inputs = tokenizer(data, return_tensors='pt', padding=True, truncation=True)
+outputs = model(**model_inputs)
+
+print('\nFinal model outputs: {}'.format(outputs))
+
