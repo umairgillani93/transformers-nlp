e06498d57b51a223e1ee75a96a04c6a9bbb27589
umairgillani93 <umairgillani93@gmail.com>
2022-10-05 03:41:23 +0500

    BERT NSP model

---
+++ nlp_for_transformers/2-fine_tunning/nsp.py
@@ -0,0 +1,51 @@
+import os
+import torch
+import torchinfo
+import pprint
+import numpy as np 
+import pandas as pd 
+from transformers import pipeline
+from transformers import AutoTokenizer
+from transformers import AutoModelForSequenceClassification
+from transformers import Trainer
+from transformers import TrainingArguments
+from datasets import load_dataset
+from datasets import load_metric
+import warnings
+warnings.filterwarnings('ignore')
+
+
+print('Imports successful')
+device = torch.device('cuda') if torch.cuda.is_available() else 'cpu'
+
+print('\nDevice: {}'.format(device))
+
+raw_dataset = load_dataset('glue', 'rte')
+print(raw_dataset['train'].features)
+
+print('\nRaw dataset: {}'.format(raw_dataset))
+
+def save_model(path, model, chkpt):
+  '''
+  downloads and saves the model from Huggingface.co
+  '''
+  print('\n >> Downloading model ..')
+  model_ = model.from_pretrained(chkpt)
+  print('\n >> Saving model to path: {}'.format(path))
+  model_.save_pretrained(path)
+  print('\nMOdel saved!')
+
+
+
+# prettry printer for python
+pp = pprint.PrettyPrinter(indent=4)
+
+# define checkpoint
+# remember! this should be same for both 'Tokenizer' and 'Model'
+checkpoint = 'distilbert-base-uncased'
+# path to save the tokenizer
+tokenizer_path = os.getenv('HOME') + '/tokenizers/AutoTokenizer/'
+model_path = os.getenv('HOME') + '/models/AutoModelForSequenceClassification_distilbert_pretrained/'
+
+# download and save the model
+save_model(model_path, AutoModelForSequenceClassification, checkpoint)
